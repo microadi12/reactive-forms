7273433b357f6ad7217a64d999b02e10192eeb60
aditya <singhaditya0407@gmail.com>
2021-02-22 01:53:02 +0530

    add nominee form added

--- src/app/create-folio/add-nominee/add-nominee.component.ts
+++ src/app/create-folio/add-nominee/add-nominee.component.ts
@@ -1,21 +1,55 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from "@angular/core";
+import {
+  FormGroup,
+  FormControl,
+  ReactiveFormsModule,
+  Validators
+} from "@angular/forms";
+import { VariableAst } from "@angular/compiler";
 
 @Component({
-  selector: 'app-add-nominee',
-  templateUrl: './add-nominee.component.html',
-  styleUrls: ['./add-nominee.component.scss']
+  selector: "app-add-nominee",
+  templateUrl: "./add-nominee.component.html",
+  styleUrls: ["./add-nominee.component.scss"]
 })
 export class AddNomineeComponent implements OnInit {
+  myForm: FormGroup;
 
-  @Input() data : any;
-  @Output() dataDetails = new EventEmitter();
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  
-    console.log('Its works');
+    this.myForm = new FormGroup({
+      firstName: new FormControl("", Validators.required),
+      relation: new FormControl("", Validators.required),
+      dob: new FormControl("", Validators.required),
+      allocation: new FormControl("", Validators.required)
+    });
 
+   this.setDate(); 
+  }
+  
+  setDate() {
+    this.myForm.patchValue({ dob: "1996-12-04" });
   }
 
+  addNominee() {
+    console.log(this.myForm);
+    console.log(this.myForm.value.firstName);
+    console.log(this.myForm.value.relation);
+    console.log(this.myForm.value.dob);
+    console.log(this.myForm.value.allocation);
+    console.log(this.myForm.status);
+    if(this.myForm.value.allocation < 100 || this.myForm.value.allocation > 100){
+      window.alert('Allocation should be 100%')
+    } else {
+      window.alert('Great Congratulation!!!')
+    }
+
+    this.clearForm();
+  }
+
+  clearForm() {
+    this.myForm.reset();
+  }
+ 
 }
--- src/app/create-folio/create-folio.module.ts
+++ src/app/create-folio/create-folio.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 import { CreateFolioRoutingModule } from './create-folio-routing.module';
 import { CreateFolioComponent } from './create-folio.component';
@@ -12,7 +12,8 @@ import { AddNomineeComponent } from './add-nominee/add-nominee.component';
   imports: [
     CommonModule,
     CreateFolioRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class CreateFolioModule { }
